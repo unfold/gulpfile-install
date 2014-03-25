4298df5a81e2ba40542e55cdd67589f3b646ccf8
Einar Löve <einar.love@gmail.com>
2014-03-25 17:11:48 +0100

    Initial commit

---
+++ bin/gulpfile-install.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+// TODO: Optimize regex to match only one group
+// Matches both 'require(..)' and whats inside parenthesis
+var regex = /require\((?:'|")(.*?)(?:'|")\)/g
+
+// Take gulpfile as an argument or use the one in current directory
+var gulpfilePath = process.argv[2] || 'gulpfile.js'
+
+
+require('fs').readFile(gulpfilePath , 'utf8', function(err, data){
+  var modules = []
+
+  while(match = regex.exec(data)){
+    modules.push(match[1])
+  }
+
+  var commands = modules.map(function(command){
+    return 'npm install ' + command + ';'
+  })
+
+  console.log('\n\nInstalling modules: \n\t' + modules.join('\n\t') + '\n\n')
+
+  require('child_process').exec(commands.join('')).stderr.pipe(process.stderr)
+})
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Gulpfile-install
+> Gulpfile-install is a command line application that scans your gulpfile.js and install all dependencies.
+
+## Example
+If your starting a new project and want to reuse your previous gulpfile:
+```
+$ mkdir new-project
+$ cp previous-project/gulpfile.js new-project
+$ cd new-project
+$ gulpfile-install
+```
+
+## Install
+Install with [npm](https://npmjs.org/package/gulpfile-install)
+```
+$ npm install -g gulpfile-install
+```
