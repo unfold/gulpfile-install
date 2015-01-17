8326de68a550ef252413a3144e00dde896eb2fe3
leolower <Leonardo.Lower@wmg.com>
2015-01-17 13:25:34 -0300

    Adds a save option to add the installed dependencies to the packages.json of the project.

--- bin/gulpfile-install.js
+++ bin/gulpfile-install.js
@@ -5,8 +5,20 @@
 var regex = /require\((?:'|")(.*?)(?:'|")\)/g
 
 // Take gulpfile as an argument or use the one in current directory
-var gulpfilePath = process.argv[2] || 'gulpfile.js'
+var gulpfilePath = 'gulpfile.js'
+var save = ""
 
+var save_values = ['-s', '--save-dev']
+process.argv.forEach(function(val, index, array) {
+    var save_index = save_values.indexOf(val)
+    if (save_index >= 0) {
+        save = '--save-dev'
+    }
+
+    if(val.match(/gulpfile.js/gi)){
+        gulpfilePath = val
+    }
+});
 
 require('fs').readFile(gulpfilePath , 'utf8', function(err, data){
   var modules = []
@@ -16,7 +28,7 @@ require('fs').readFile(gulpfilePath , 'utf8', function(err, data){
   }
 
   var commands = modules.map(function(command){
-    return 'npm install ' + command + ';'
+    return 'npm install ' + command + ' '+ save + ';'
   })
 
   console.log('\n\nInstalling modules: \n\t' + modules.join('\n\t') + '\n\n')
